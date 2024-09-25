60aa4bf6831b0631d44a7324ae3397057f2de8e5
000M000 <134954140+000M000@users.noreply.github.com>
2024-09-25 13:03:57 -0230

    Add files via upload

---
+++ backend/api/data_trends/display_graphs.js
@@ -0,0 +1,24 @@
+const matplotlib = require('matplotlib');
+
+class DisplayGraphs {
+  constructor(dataPoints, xAxisName, yAxisName) {
+    this.dataPoints = dataPoints;
+    this.xAxisName = xAxisName;
+    this.yAxisName = yAxisName;
+  }
+
+  makeGraph() {
+    const plt = matplotlib.pyplot;
+    const dates = this.dataPoints.map(dp => dp.date);
+    const prices = this.dataPoints.map(dp => dp.price);
+
+    plt.plot(dates, prices, marker='o');
+    plt.xlabel(this.xAxisName);
+    plt.ylabel(this.yAxisName);
+    plt.title('Price Trends');
+    plt.xticks(rotation=45);
+    plt.show();
+  }
+}
+
+module.exports = DisplayGraphs;
