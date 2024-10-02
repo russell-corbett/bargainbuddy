92bf20e4bd4539221cef7f8cbc3dd6ae91177e22
Russell Corbett <russellcorbett11@gmail.com>
2024-10-02 16:06:05 -0230

    Update Versions

--- backend/index.js
+++ backend/index.js
@@ -23,8 +23,8 @@ const firebaseConfig = {
 // Initialize Firebase
 
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 
 const express = require("express");
