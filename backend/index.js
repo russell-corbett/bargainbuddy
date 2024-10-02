d83f375ccfbf457d4fd76c8a4a83ee91d90e95d9
Jesse <jesserl@mun.ca>
2024-10-02 14:44:55 -0230

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA86tDTFZ3MlfOnkOIVC5-rEJv3eIKKe8Y",
+  authDomain: "bargainbuddy-99abb.firebaseapp.com",
+  projectId: "bargainbuddy-99abb",
+  storageBucket: "bargainbuddy-99abb.appspot.com",
+  messagingSenderId: "735437299454",
+  appId: "1:735437299454:web:f37a51ceeeef48a291cfaf",
+  measurementId: "G-LQKS57DWDE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
