9b7456324632406690aac5656e547a9831e14953
Jacob Norman <janorman@mun.ca>
2024-10-20 12:40:43 -0230

    Can now use the "/addPrice" route to create a Price model. Needs to be tested but adding now for group view

--- backend/routes/priceRoutes.js
+++ backend/routes/priceRoutes.js
@@ -3,7 +3,9 @@ const priceController = require("../controllers/priceController");
 
 const router = express.Router();
 
+// Creates a new Price model, requires store, price, and itemId in the body
 router.post("/addPrice", priceController.addPrice);
-router.post("/getPrices", priceController.getPrices);
+
+router.post("/getPrices", priceController.getPrices); // Needs to be made
 
 module.exports = router;
