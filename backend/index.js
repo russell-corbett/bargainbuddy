04f2c5ef91cb61f5e40fa39e5260841a9ecd3384
Russell Corbett <russellcorbett11@gmail.com>
2024-10-25 12:04:53 -0230

    Corrected BestBuy Path

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const userRoutes = require("./api/endpoints/user/userRoutes");
 const itemRoutes = require("./api/endpoints/item/itemRoutes");
 const priceRoutes = require("./api/endpoints/price/priceRoutes");
 const trendRoutes = require("./api/endpoints/trend/trendRoutes");
-const bestBuyApi = require("../backend//microServices//BestBuyService");
+const BestBuyService = require("./microServices/BestBuyService"); // Corrected path
 
 const app = express();
 const port = 3001;
@@ -21,7 +21,7 @@ const io = socketIO(server, {
 
 const db = Database;
 
-const bestBuyApiRef = new bestBuyApi();
+const bestBuyApiRef = new BestBuyService();
 
 // Enable CORS for all routes
 app.use(cors());
